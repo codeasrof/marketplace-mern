f4682b3d6b2934571d71eeb95f6fec7ddb4eba30
codeasrof <vitalidade.08-devorador@icloud.com>
2023-10-14 13:24:05 -0300

    Adding MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .catch((err) => {
+        console.log(`An error was occured: ${err}`)
+    })
+    
 const app = express()
 
 
