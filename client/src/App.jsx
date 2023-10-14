32d226198c25397c1372e686df93e6ad53a97ee0
codeasrof <vitalidade.08-devorador@icloud.com>
2023-10-13 21:43:26 -0300

    creating pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>
-        Hello tailwind</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-out" element={<SignUp/>}/>
+        <Route path="/sign-out" element={<About/>}/>
+        <Route path="/sign-out" element={<Profile/>}/>
+      </Routes>
+    </Router>
   )
 }
 
