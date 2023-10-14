802ebfd23584dd8e0d903c19c61fcd2d25c69334
codeasrof <vitalidade.08-devorador@icloud.com>
2023-10-14 16:28:44 -0300

    Error handling

--- api/controllers/Auth.controller.js
+++ api/controllers/Auth.controller.js
@@ -1,14 +1,17 @@
 import User from "../models/User.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js"
 
-export const Signup = async (req,res) => {
+export const Signup = async (req,res,next) => {
     const {username, email, password} = await req.body
     const hashedPassword = bcryptjs.hashSync(password, 16)
     const newUser = new User({username, email, password: hashedPassword})
     try {
+
         await newUser.save()
         res.status(201).json({message: "User created!"})
+        
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 }
