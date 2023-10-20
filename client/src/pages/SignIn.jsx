151aefd14cc1c7cdbdc90af87b5d575ac6c05531
codeasrof <vitalidade.08-devorador@icloud.com>
2023-10-19 21:06:40 -0300

    initializing login screen

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+      <section className='sm:flex relative sm:static'>
+        <div 
+          className='
+            flex 
+            items-center 
+            justify-center 
+            bg-[url("src/assets/login_building.jpg")] 
+            h-screen w-[100vw] object-cover
+            bg-center
+            sm:w-[60vw] sm:object-contain
+          '
+        >
+        </div>
+
+        <div 
+          className='
+            flex-col 
+            justify-center 
+            w-[100vw]
+            absolute
+            top-0
+            bg-neutral-950/[.8]
+            h-[100vh]
+            
+            sm:w-[40vw] 
+            sm:bg-slate-100
+            sm:h-screen
+            sm:flex-col
+            sm:static
+
+            '
+        >
+            <h1>Sign In</h1>
+
+            <form>
+              
+            </form>
+        </div>
+      </section>
   )
 }
 
